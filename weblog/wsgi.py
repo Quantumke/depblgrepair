273117f2e5647c298af64f1673007e1564586fce
quantumke <nguruben@gmail.com>
2016-09-18 13:06:21 +0300

    #8

--- weblog/wsgi.py
+++ weblog/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append('/opt/python/current/app/')
+sys.path.append('/opt/python/current/app/')
+sys.executable = '/usr/local/python-2.7.2/bin/python'
 
 from django.core.wsgi import get_wsgi_application
 
