7705614e35017b5d891548358e48682139c2223e
quantumke <nguruben@gmail.com>
2016-09-18 12:22:27 +0300

    yay

--- weblog/settings.py
+++ weblog/settings.py
@@ -78,10 +78,10 @@ if 'RDS_DB_NAME' in os.environ:
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['RDS_DB_NAME'],
-            'USER': 'postgres',
-            'PASSWORD': 'master12!',
-            'HOST': 'localhost',
-            'PORT': '5432',
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
         }
     }
 else:
@@ -89,7 +89,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'ishiblog',
-            'USER': 'postgres',
+            'USER': 'root',
             'PASSWORD': 'master12!',
             'HOST': 'localhost',
             'PORT': '5432',
